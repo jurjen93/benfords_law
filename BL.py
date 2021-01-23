75da9724645bb34927115243dded8db80b94d44f
Jurjen de Jong <jurjendejong93@gmail.com>
2021-01-23 16:45:28 +0100

    Update BL.py

--- BL.py
+++ BL.py
@@ -1,55 +1,123 @@
 import matplotlib.pyplot as plt
-from math import log10, nan, sqrt
+from math import log10, sqrt, log
 from matplotlib import style
 from scipy.spatial import distance
+from collections import Counter
 
-"""This is a very simple script to test Benford's law 
-on your data where we use Poisson error bars and calculate 
-the normalized Euclidean Distance function.
-The data has to be converted to a pandas dataframe."""
+class BenfordsLaw:
+    def __init__(self, data):
+        self.data = data
 
-def bl_analysis(df, fields):
-    def bl(n):
+    @staticmethod
+    def benford_formula(n: int):
         return log10(1 + 1 / n)
 
-    BL_frequencies = {i: bl(i) for i in range(1, 10)}
+    @staticmethod
+    def first_n_digits(num, n: int=1):
+        first_digits = int(abs(num) // 10 ** (int(log(abs(num), 10)) - n + 1))
+        if n==1:
+            return first_digits
+        elif n==0:
+            print('Cannot have n equal to 0')
+        else:
+            return int(str(first_digits)[-1])
 
-    def ed_bl(frequencies):
-        return round(distance.euclidean(list(BL_frequencies.values()), frequencies) / distance.euclidean(
-            list(BL_frequencies.values()), [0] * 8 + [1]), 3)
+    @property
+    def first_benford_frequencies(self):
+        return {i: self.benford_formula(i) for i in range(1, 10)}
 
-    def above_one(inp):
-        try:
-            inp = float(inp)
-            if inp == 0:
-                return nan
-            while inp < 1:
-                inp *= 10
-            return inp
-        except TypeError:
-            return nan
+    @property
+    def second_benford_frequencies(self):#should be improved with formula
+        return {0: 0.12, 1: 0.114, 2: 0.109, 3: 0.104, 4: 0.100, 5: 0.097, 6: 0.093, 7: 0.090, 8: 0.088, 9: 0.085}
 
-    for field in fields:
-        error_bar = 1/sqrt(len(df[field].dropna()))
-        print(error_bar)
-        df_frequencies = {int(i):j for i,j in (df[field].apply(lambda x: above_one(x)).dropna().astype(str).str[
-                              0].value_counts().sort_index() / len(df)).to_dict().items()}
+    @property
+    def third_benford_frequencies(self):#should be improved with formula
+        return {0: 0.102, 1: 0.101, 2: 0.101, 3: 0.101, 4: 0.100, 5: 0.100, 6: 0.099, 7: 0.099, 8: 0.099, 9: 0.098}
+
+    def first_euclidean_distance(self, frequencies):
+        return round(distance.euclidean(list(self.first_benford_frequencies.values()), frequencies) / distance.euclidean(
+            list(self.first_benford_frequencies.values()), [0] * 8 + [1]), 3)
+
+    def second_euclidean_distance(self, frequencies):
+        return round(distance.euclidean(list(self.second_benford_frequencies.values()), frequencies) / distance.euclidean(
+            list(self.second_benford_frequencies.values()), [0] * 9 + [1]), 3)
+
+    def third_euclidean_distance(self, frequencies):
+        return round(distance.euclidean(list(self.third_benford_frequencies.values()), frequencies) / distance.euclidean(
+            list(self.third_benford_frequencies.values()), [0] * 9 + [1]), 3)
+
+    @property
+    def first_digit_frequencies(self):
+        count = Counter([self.first_n_digits(d, 1) for d in self.data])
+        return {int(i):j/sum(count.values()) for i,j in count.items() if i!=0}
+
+    @property
+    def second_digit_frequencies(self):
+        count = Counter([self.first_n_digits(d, 2) for d in self.data])
+        return {int(i):j/sum(count.values()) for i,j in count.items()}
+
+    @property
+    def third_digit_frequencies(self):
+        count = Counter([self.first_n_digits(d, 3) for d in self.data])
+        return {int(i):j/sum(count.values()) for i,j in count.items()}
+
+    def plot_first_digit(self, name: str='', save_as: str='', show: bool=True):
+        error_bar = 1 / sqrt(len(self.data)) #poisson error bars -> could be improved with other error bars
         style.use('ggplot')
-        plt.scatter(BL_frequencies.keys(), BL_frequencies.values(), marker='s', color='black')
-        plt.scatter(df_frequencies.keys(), df_frequencies.values(), marker='o')
-        plt.errorbar(df_frequencies.keys(), df_frequencies.values(), yerr=error_bar, fmt='o')
+        plt.scatter(self.first_benford_frequencies.keys(), self.first_benford_frequencies.values(), marker='s', color='black')
+        plt.scatter(self.first_digit_frequencies.keys(), self.first_digit_frequencies.values(), marker='o')
+        plt.errorbar(self.first_digit_frequencies.keys(), self.first_digit_frequencies.values(), yerr=error_bar, fmt='o')
         plt.legend(["Benford's law", "First significant digit frequency data"])
-        plt.title(f"{field.title().replace('_', ' ')} has ED:\n{ed_bl(list(df_frequencies.values()))}", size=16)
+        plt.title(f"{name.title().replace('_', ' ')} has ED:\n{self.first_euclidean_distance(list(self.first_digit_frequencies.values()))}", size=16)
         plt.xticks(size=16)
         plt.yticks(plt.yticks()[0], [int(round(p, 2) * 100) for p in plt.yticks()[0]], size=16)
         plt.xlabel('First significant digit', size=16)
         plt.ylabel('Frequency (%)', size=16)
-        plt.show()
+        if show:
+            plt.show()
+        if save_as:
+            plt.savefig(f'{save_as}')
+        return self
+
+    def plot_second_digit(self, name: str='', save_as: str='', show: bool=True):
+        error_bar = 1 / sqrt(len(self.data)) #poisson error bars -> could be improved with other error bars
+        style.use('ggplot')
+        plt.scatter(self.second_benford_frequencies.keys(), self.second_benford_frequencies.values(), marker='s', color='black')
+        plt.scatter(self.second_digit_frequencies.keys(), self.second_digit_frequencies.values(), marker='o')
+        plt.errorbar(self.second_digit_frequencies.keys(), self.second_digit_frequencies.values(), yerr=error_bar, fmt='o')
+        plt.legend(["Benford's law", "First significant digit frequency data"])
+        plt.title(f"{name.title().replace('_', ' ')} has ED:\n{self.second_euclidean_distance(list(self.second_digit_frequencies.values()))}", size=16)
+        plt.xticks(size=16)
+        plt.yticks(plt.yticks()[0], [int(round(p, 2) * 100) for p in plt.yticks()[0]], size=16)
+        plt.xlabel('First significant digit', size=16)
+        plt.ylabel('Frequency (%)', size=16)
+        if show:
+            plt.show()
+        if save_as:
+            plt.savefig(f'{save_as}')
+        return self
+
+    def plot_third_digit(self, name: str='', save_as: str='', show: bool=True):
+        error_bar = 1 / sqrt(len(self.data)) #poisson error bars -> could be improved with other error bars
+        style.use('ggplot')
+        plt.scatter(self.third_benford_frequencies.keys(), self.third_benford_frequencies.values(), marker='s', color='black')
+        plt.scatter(self.third_digit_frequencies.keys(), self.third_digit_frequencies.values(), marker='o')
+        plt.errorbar(self.third_digit_frequencies.keys(), self.third_digit_frequencies.values(), yerr=error_bar, fmt='o')
+        plt.legend(["Benford's law", "First significant digit frequency data"])
+        plt.title(f"{name.title().replace('_', ' ')} has ED:\n{self.third_euclidean_distance(list(self.third_digit_frequencies.values()))}", size=16)
+        plt.xticks(size=16)
+        plt.yticks(plt.yticks()[0], [int(round(p, 2) * 100) for p in plt.yticks()[0]], size=16)
+        plt.xlabel('First significant digit', size=16)
+        plt.ylabel('Frequency (%)', size=16)
+        if show:
+            plt.show()
+        if save_as:
+            plt.savefig(f'{save_as}')
+        return self
 
 if  __name__=="__main__":
     #example:
-    from pandas import DataFrame
     from random import uniform
-    random_dict = {i: uniform(0, 10) for i in range(1000)}
-    df = DataFrame([random_dict]).transpose().rename(columns={0: 'example_field'})
-    bl_analysis(df, ['example_field'])
+    random_data = [uniform(-10, 10) for i in range(0,1000)]
+    bl = BenfordsLaw(random_data)
+    bl.plot_first_digit()
