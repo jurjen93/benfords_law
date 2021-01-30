718e4b5a3d495eb554c2d1955b94fc0f9ee02dae
Jurjen de Jong <jurjendejong93@gmail.com>
2021-01-30 15:42:16 +0100

    readme

--- README.md
+++ README.md
@@ -10,24 +10,38 @@ Benford's law might be helpful to detect [fraud], do [science], or just investig
 By ```pip install benfordslaw-analysis``` you will install the package.
 
 #### Usage
-Now you can do ```from benfordslaw_analysis import analysis``` to obtain the analysis script.
-Here there is the class ```BenfordsLaw``` which you can get with ```analysis.BenfordsLaw```.
-Now you can analyse your data by making plots.
+Now you can do ```from benfordslaw_analysis.analysis import Analysis```.
+You have now imported the class ```Analysis```.
+Now you can play around with your data and test if Benford's law is hidden in your data.
 
 For example, make a plot with Benford's law versus random data with:
 ```
-from benfordslaw_analysis.analysis import BenfordsLaw
+from benfordslaw_analysis.analysis import Analysis
 from random import uniform
 random_data = [uniform(-10, 10) for i in range(0,1000)]
-bl = BenfordsLaw(random_data)
-bl.plot_first_digit()
+bl = Analysis(random_data)
+bl.plot_first_digit('Random stuff')
 ```
-Note that we use the Euclidean distance between the digit frequency from Benford's law and your own data as a measure
-and that we use Poisson error bars.
+![Test Image 1](test.png)
 
-This package is still under development. More updates and documentation will come...
+Note that we use the [Euclidean distance] between the digit frequency from Benford's law and your own data as a measure
+and that we use Poisson error bars (based on the number of data points).
+
+#### Citing
+
+If you find ```benfordslaw_analysis``` a useful tool for your own research, please cite in the following way:
+
+```
+@misc{benford_py,
+      author = {Jurjen, de Jong},
+      title = {{benfordslaw_analysis: a Python Implementation of Benford's Law analysis}},
+      year = {2021},
+      howpublished = {\url{https://github.com/jurjen93/Benfords_law}},
+}
+```
 
 [wikipedia]: https://en.wikipedia.org/wiki/Benford%27s_law
 [fraud]: https://www.journalofaccountancy.com/issues/2017/apr/excel-and-benfords-law-to-detect-fraud.html
 [science]: https://towardsdatascience.com/benfords-law-in-the-gaia-universe-b5727db7a936
 [quality of data]: https://www.idfcinstitute.org/blog/2020/november/using-benfords-law-to-understand-covid-19-data-quality/
+[Euclidean distance]: https://en.wikipedia.org/wiki/Euclidean_distance
--- benfordslaw_analysis/analysis.py
+++ benfordslaw_analysis/analysis.py
@@ -5,46 +5,79 @@ from scipy.spatial import distance
 from collections import Counter
 
 class BenfordsLaw:
-    def __init__(self, data):
-        self.data = data
+    """
+    BenfordsLaw is a class to obtain benford properties:
+    ----------
+    first_benford_frequencies -> return the frequencies of benford's law for first significant digits
+    second_benford_frequencies -> return the frequencies of benford's law for second significant digits
+    third_benford_frequencies -> return the frequencies of benford's law for third significant digits
+    ----------
+    """
 
     @staticmethod
     def benford_formula(n: int):
         return log10(1 + 1 / n)
-
-    @staticmethod
-    def first_n_digits(num, n: int=1):
-        first_digits = int(abs(num) // 10 ** (int(log(abs(num), 10)) - n + 1))
-        if n==1:
-            return first_digits
-        elif n==0:
-            print('Cannot have n equal to 0')
-        else:
-            return int(str(first_digits)[-1])
 
     @property
     def first_benford_frequencies(self):
         return {i: self.benford_formula(i) for i in range(1, 10)}
 
     @property
-    def second_benford_frequencies(self):#should be improved with formula
+    def second_benford_frequencies(self):  # should be improved with formula
         return {0: 0.12, 1: 0.114, 2: 0.109, 3: 0.104, 4: 0.100, 5: 0.097, 6: 0.093, 7: 0.090, 8: 0.088, 9: 0.085}
 
     @property
-    def third_benford_frequencies(self):#should be improved with formula
+    def third_benford_frequencies(self):  # should be improved with formula
         return {0: 0.102, 1: 0.101, 2: 0.101, 3: 0.101, 4: 0.100, 5: 0.100, 6: 0.099, 7: 0.099, 8: 0.099, 9: 0.098}
 
-    def first_euclidean_distance(self, frequencies):
-        return round(distance.euclidean(list(self.first_benford_frequencies.values()), frequencies) / distance.euclidean(
-            list(self.first_benford_frequencies.values()), [0] * 8 + [1]), 3)
+class Analysis(BenfordsLaw):
+    """
+    Analysis is a class to analyse the first digits of your data.
+    You can do the following things:
 
-    def second_euclidean_distance(self, frequencies):
-        return round(distance.euclidean(list(self.second_benford_frequencies.values()), frequencies) / distance.euclidean(
-            list(self.second_benford_frequencies.values()), [0] * 9 + [1]), 3)
+    get the following properties in dictionary form:
+    ----------
+    first_digit_frequencies
+    second_digit_frequencies
+    third_digit_frequencies
+    ----------
 
-    def third_euclidean_distance(self, frequencies):
-        return round(distance.euclidean(list(self.third_benford_frequencies.values()), frequencies) / distance.euclidean(
-            list(self.third_benford_frequencies.values()), [0] * 9 + [1]), 3)
+    get the following measures:
+    ----------
+    first_euclidean_distance -> return the euclidean distance between your data's first significant data frequencies and benford frequencies
+    second_euclidean_distance -> return the euclidean distance between your data's second significant data frequencies and benford frequencies
+    third_euclidean_distance -> return the euclidean distance between your data's third significant data frequencies and benford frequencies
+    ----------
+
+    plot the following:
+    ----------
+    plot_first_digit -> plot the first significant digit frequency versus benford's law with the euclidean distance in the title
+    plot_second_digit ->  plot the second significant digit frequency versus benford's law with the euclidean distance in the title
+    plot_third_digit -> plot the third significant digit frequency versus benford's law with the euclidean distance in the title
+    ----------
+    """
+
+    def __init__(self, data: list=None):
+        self.data = data
+
+    @staticmethod
+    def first_n_digits(num, n: int = 1):
+        """
+        Get the first n digits from a number.
+
+        Parameters
+        ----------
+        num (float) : number
+        n (int)     : number of first digits
+        ----------
+        """
+        first_digits = int(abs(num) // 10 ** (int(log(abs(num), 10)) - n + 1))
+        if n == 1:
+            return first_digits
+        elif n == 0:
+            print('Cannot have input equal to 0')
+        else:
+            return int(str(first_digits)[-1])
 
     @property
     def first_digit_frequencies(self):
@@ -61,7 +94,30 @@ class BenfordsLaw:
         count = Counter([self.first_n_digits(d, 3) for d in self.data])
         return {int(i):j/sum(count.values()) for i,j in count.items()}
 
+    def first_euclidean_distance(self, frequencies):
+        return round(distance.euclidean(list(self.first_benford_frequencies.values()), frequencies) / distance.euclidean(
+            list(self.first_benford_frequencies.values()), [0] * 8 + [1]), 3)
+
+    def second_euclidean_distance(self, frequencies):
+        return round(distance.euclidean(list(self.second_benford_frequencies.values()), frequencies) / distance.euclidean(
+            list(self.second_benford_frequencies.values()), [0] * 9 + [1]), 3)
+
+    def third_euclidean_distance(self, frequencies):
+        return round(distance.euclidean(list(self.third_benford_frequencies.values()), frequencies) / distance.euclidean(
+            list(self.third_benford_frequencies.values()), [0] * 9 + [1]), 3)
+
     def plot_first_digit(self, name: str='', save_as: str='', show: bool=True):
+        """
+        Plot the first significant digit frequency of your data versus benford's law.
+        We use the Euclidean distance as measure and poisson error bars.
+
+        Parameters
+        ----------
+        name (str): name of your data
+        save_as (str): name for your image if you want to save
+        show (bool): if you want to show on screen or not
+        ----------
+        """
         error_bar = 1 / sqrt(len(self.data)) #poisson error bars -> could be improved with other error bars
         style.use('ggplot')
         plt.scatter(self.first_benford_frequencies.keys(), self.first_benford_frequencies.values(), marker='s', color='black')
@@ -73,6 +129,7 @@ class BenfordsLaw:
         plt.yticks(plt.yticks()[0], [int(round(p, 2) * 100) for p in plt.yticks()[0]], size=16)
         plt.xlabel('First significant digit', size=16)
         plt.ylabel('Frequency (%)', size=16)
+        plt.tight_layout()
         if show:
             plt.show()
         if save_as:
@@ -80,6 +137,17 @@ class BenfordsLaw:
         return self
 
     def plot_second_digit(self, name: str='', save_as: str='', show: bool=True):
+        """
+        Plot the second significant digit frequency of your data versus benford's law.
+        We use the Euclidean distance as measure and poisson error bars.
+
+        Parameters
+        ----------
+        name (str): name of your data
+        save_as (str): name for your image if you want to save
+        show (bool): if you want to show on screen or not
+        ----------
+        """
         error_bar = 1 / sqrt(len(self.data)) #poisson error bars -> could be improved with other error bars
         style.use('ggplot')
         plt.scatter(self.second_benford_frequencies.keys(), self.second_benford_frequencies.values(), marker='s', color='black')
@@ -91,6 +159,7 @@ class BenfordsLaw:
         plt.yticks(plt.yticks()[0], [int(round(p, 2) * 100) for p in plt.yticks()[0]], size=16)
         plt.xlabel('First significant digit', size=16)
         plt.ylabel('Frequency (%)', size=16)
+        plt.tight_layout()
         if show:
             plt.show()
         if save_as:
@@ -98,6 +167,17 @@ class BenfordsLaw:
         return self
 
     def plot_third_digit(self, name: str='', save_as: str='', show: bool=True):
+        """
+        Plot the third significant digit frequency of your data versus benford's law.
+        We use the Euclidean distance as measure and poisson error bars.
+
+        Parameters
+        ----------
+        name (str): name of your data
+        save_as (str): name for your image if you want to save
+        show (bool): if you want to show on screen or not
+        ----------
+        """
         error_bar = 1 / sqrt(len(self.data)) #poisson error bars -> could be improved with other error bars
         style.use('ggplot')
         plt.scatter(self.third_benford_frequencies.keys(), self.third_benford_frequencies.values(), marker='s', color='black')
@@ -109,6 +189,7 @@ class BenfordsLaw:
         plt.yticks(plt.yticks()[0], [int(round(p, 2) * 100) for p in plt.yticks()[0]], size=16)
         plt.xlabel('First significant digit', size=16)
         plt.ylabel('Frequency (%)', size=16)
+        plt.tight_layout()
         if show:
             plt.show()
         if save_as:
@@ -119,5 +200,5 @@ if  __name__=="__main__":
     #example:
     from random import uniform
     random_data = [uniform(-10, 10) for i in range(0,1000)]
-    bl = BenfordsLaw(random_data)
-    bl.plot_first_digit()
+    bl = Analysis(random_data)
+    bl.plot_first_digit('Random stuff')
